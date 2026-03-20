050e5064a63b9f86f342f7d6a0f962119dcb455f
Ben Meyer <88737362+benjaminsmeyer@users.noreply.github.com>
2026-03-20 16:54:16 -0400

    ch5 lab 5

--- Lab5/index.js
+++ Lab5/index.js
@@ -2,7 +2,11 @@ import PathParameters from "./PathParameters.js";
 import QueryParameters from "./QueryParameters.js";
 import WorkingWithArrays from "./WorkingWithArrays.js";
 import WorkingWithObjects from "./WorkingWithObjects.js";
+import cors from "cors";
+import express from "express";
 export default function Lab5(app) {
+  app.use(cors());
+  app.use(express.json());
   app.get("/lab5/welcome", (req, res) => {
     res.send("Welcome to Lab 5");
   });
