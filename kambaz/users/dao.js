4207aaf025f21d9ed2675f701336834d3e6af057
Ben Meyer <88737362+benjaminsmeyer@users.noreply.github.com>
2026-03-23 19:39:46 -0400

    people table

--- kambaz/users/dao.js
+++ kambaz/users/dao.js
@@ -1,27 +1,61 @@
 import { v4 as uuidv4 } from "uuid";
 export default function UsersDao(db) {
-  let { users } = db;
   const createUser = (user) => {
     const newUser = { ...user, _id: uuidv4() };
-    users = [...users, newUser];
+    db.users = [...db.users, newUser];
     return newUser;
   };
+
   const findUserByUsername = (username) =>
-    users.find((user) => user.username === username);
-  const findAllUsers = () => users;
-  const findUserById = (userId) => users.find((user) => user._id === userId);
+    db.users.find((user) => user.username === username);
+
+  const findAllUsers = () => db.users;
+
+  const findUserById = (userId) => db.users.find((user) => user._id === userId);
+
   const findUserByCredentials = (username, password) =>
-    users.find(
+    db.users.find(
       (user) => user.username === username && user.password === password,
     );
-  const updateUser = (userId, user) =>
-    (users = users.map((u) => (u._id === userId ? user : u)));
-  const deleteUser = (userId) =>
-    (users = users.filter((u) => u._id !== userId));
+
+  const findUsersForCourse = (courseId) => {
+    const enrolledUserIds = new Set(
+      db.enrollments
+        .filter((enrollment) => enrollment.course === courseId)
+        .map((enrollment) => enrollment.user),
+    );
+    return db.users.filter((user) => enrolledUserIds.has(user._id));
+  };
+
+  const updateUser = (userId, userUpdates) => {
+    let updatedUser = null;
+    db.users = db.users.map((user) => {
+      if (user._id !== userId) {
+        return user;
+      }
+      updatedUser = { ...user, ...userUpdates, _id: userId };
+      return updatedUser;
+    });
+    return updatedUser;
+  };
+
+  const deleteUser = (userId) => {
+    const user = findUserById(userId);
+    if (!user) {
+      return null;
+    }
+    db.users = db.users.filter((u) => u._id !== userId);
+    db.enrollments = db.enrollments.filter(
+      (enrollment) => enrollment.user !== userId,
+    );
+    return user;
+  };
+
   return {
     createUser,
     findAllUsers,
     findUserById,
+    findUsersForCourse,
     findUserByUsername,
     findUserByCredentials,
     updateUser,
