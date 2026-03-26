ee2ce66e7ce5719487d853f8254b790b0ee2db27
Ben Meyer <88737362+benjaminsmeyer@users.noreply.github.com>
2026-03-25 21:21:02 -0400

    dao modules change

--- kambaz/modules/dao.js
+++ kambaz/modules/dao.js
@@ -1,59 +1,81 @@
 import { v4 as uuidv4 } from "uuid";
+import CourseModel from "../courses/model.js";
+import seedModules from "../database/modules.js";
 
 export default function ModulesDao(db) {
-  const findAllModules = () => db.modules;
-  const findModuleById = (moduleId) =>
-    db.modules.find((module) => module._id === moduleId);
+  async function seedIfNeeded() {
+    const courses = await CourseModel.find({ "modules.0": { $exists: false } });
+    const modulesByCourse = {};
+    seedModules.forEach((m) => {
+      if (!modulesByCourse[m.course]) modulesByCourse[m.course] = [];
+      modulesByCourse[m.course].push({
+        _id: m._id,
+        name: m.name,
+        description: m.description,
+        lessons: m.lessons,
+      });
+    });
+    for (const course of courses) {
+      if (modulesByCourse[course._id]) {
+        await CourseModel.updateOne(
+          { _id: course._id },
+          { $set: { modules: modulesByCourse[course._id] } },
+        );
+      }
+    }
+  }
 
-  function createModule(courseId, module) {
-    const newModule = { ...module, _id: uuidv4(), course: courseId };
-    db.modules = [...db.modules, newModule];
+  async function findModulesForCourse(courseId) {
+    await seedIfNeeded();
+    const course = await CourseModel.findById(courseId);
+    if (!course) return [];
+    return course.modules;
+  }
+
+  async function findAllModules() {
+    await seedIfNeeded();
+    const courses = await CourseModel.find({});
+    return courses.flatMap((course) => course.modules);
+  }
+
+  async function findModuleById(moduleId) {
+    await seedIfNeeded();
+    const course = await CourseModel.findOne({ "modules._id": moduleId });
+    if (!course) return null;
+    return course.modules.id(moduleId);
+  }
+
+  async function createModule(courseId, module) {
+    const newModule = {
+      _id: uuidv4(),
+      name: module.name,
+      description: module.description,
+      lessons: module.lessons || [],
+    };
+    await CourseModel.updateOne(
+      { _id: courseId },
+      { $push: { modules: newModule } },
+    );
     return newModule;
   }
 
-  function updateModule(courseId, moduleId, moduleUpdates) {
-    const exactMatchExists = db.modules.some(
-      (module) =>
-        module._id === moduleId &&
-        (courseId ? module.course === courseId : true),
-    );
-    let updatedModule = null;
-    db.modules = db.modules.map((module) => {
-      if (module._id !== moduleId) {
-        return module;
-      }
-      if (exactMatchExists && courseId && module.course !== courseId) {
-        return module;
-      }
-      updatedModule = {
-        ...module,
-        ...moduleUpdates,
-        _id: moduleId,
-        course: module.course || courseId,
-      };
-      return updatedModule;
-    });
-    return updatedModule;
+  async function updateModule(courseId, moduleId, moduleUpdates) {
+    const course = await CourseModel.findById(courseId);
+    if (!course) return null;
+    const module = course.modules.id(moduleId);
+    if (!module) return null;
+    Object.assign(module, moduleUpdates);
+    await course.save();
+    return module;
   }
 
-  function deleteModule(courseId, moduleId) {
-    let deletedModule = db.modules.find(
-      (module) =>
-        module._id === moduleId &&
-        (courseId ? module.course === courseId : true),
+  async function deleteModule(courseId, moduleId) {
+    const result = await CourseModel.updateOne(
+      { _id: courseId },
+      { $pull: { modules: { _id: moduleId } } },
     );
-    if (!deletedModule) {
-      deletedModule = db.modules.find((module) => module._id === moduleId);
-    }
-    if (!deletedModule) {
-      return null;
-    }
-    db.modules = db.modules.filter((module) => module._id !== moduleId);
-    return deletedModule;
-  }
-
-  function findModulesForCourse(courseId) {
-    return db.modules.filter((module) => module.course === courseId);
+    if (result.modifiedCount === 0) return null;
+    return { _id: moduleId };
   }
 
   return {
